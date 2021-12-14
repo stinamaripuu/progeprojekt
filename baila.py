bfb636382e6960e33ba1efdbafe06e8217432838
stinamaripuu <sinamaripuu@gmail.com>
2021-12-14 18:12:09 +0200

    aa

--- baila.py
+++ baila.py
@@ -5,14 +5,14 @@ ringist kaarte ilma ringi lõhkumata.
 #randomiga ka tekst oih, ring läks katki, võta lonks
 '''
 from random import randint
-'''
+
 def randomize(algne):
     uus = []
-    for a in algne:
+    for a in range(algne):
+        print(uus[a])
     
-    
-    return uus
-'''
+    #return uus
+
 def milline(number):
     try:
         kaardid = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
@@ -28,15 +28,16 @@ kaardid = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 kõik_kaardid = ['2♣', '2♦', '2♥', '2♠', '3♣', '3♦', '3♥', '3♠', '4♣', '4♦', '4♥', '4♠', '5♣', '5♦', '5♥', '5♠', '6♣', '6♦', '6♥', '6♠', '7♣', '7♦', '7♥', '7♠', '8♣', '8♦', '8♥', '8♠', '9♣', '9♦', '9♥', '9♠', '10♣', '10♦', '10♥', '10♠', 'J♣', 'J♦', 'J♥', 'J♠', 'Q♣', 'Q♦', 'Q♥', 'Q♠', 'K♣', 'K♦', 'K♥', 'K♠', 'A♣', 'A♦', 'A♥', 'A♠']
 n = ['2♦', '3♥', '3♠', '4♣', '5♣', '6♠', '7♣', '8♠', '9♣', '10♠', 'J♣', 'Q♠', 'K♣', 'A♥']
 vahepealne = kõik_kaardid
+
 for a in vahepealne:
     try: 
         if len(a) > 2:
-            print(a[0:2])
+            #print(a[0:2])
             milline(a[0:2])
             
         else:
-            print(a[0])
+            #print(a[0])
             milline(a[0])
     except:
         break
-
+randomize(milline)
